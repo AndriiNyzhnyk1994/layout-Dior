138b1a95621302ebca421baa7fbc22d1024909df
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2024-03-27 14:55:01 +0200

    added Accordion Block

--- js/main.js
+++ js/main.js
@@ -1,23 +1,34 @@
 const swiperTop = new Swiper('.top__swiper', {
-    effect: "fade",
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    autoplay: {
-        delay: 3500,
-        disableOnInteraction: false,
-    }
-  });
+  effect: "fade",
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  }
+});
 
 
-  
-  const swiperAbout = new Swiper(".about__slider", {
-    slidesPerView: 4,
-    spaceBetween: 20,
-    freeMode: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+
+const swiperAbout = new Swiper(".about__slider", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  freeMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+document.querySelectorAll('.accordion__triger').forEach(item => {
+  item.addEventListener('click', () => {
+    item.parentNode.classList.toggle('accordion__item--active')
+  })
+})
+
+
+
+
